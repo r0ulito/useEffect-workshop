cece3f47827efb1f7aedac7867f926e796cfa996
r0ulito <roulit77@gmail.com>
2023-04-18 22:39:47 +0200

    Correction

--- src/PopularJSRepos.js
+++ src/PopularJSRepos.js
@@ -2,33 +2,45 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function PopularJSRepos() {
-  const [repos, setRepos] = useState([]);
+	const [repos, setRepos] = useState([]);
 
-  // TODO : load the repos into state when the component mounts
+	// TODO : load the repos into state when the component mounts
 
-  return (
-    <div>
-      <h2>Popular JS repos</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Stars</th>
-          </tr>
-        </thead>
-        <tbody>
-          {repos.map((r) => (
-            <tr key={r.id}>
-              <td>
-                <a target="blank" href={r.html_url}>
-                  {r.name}
-                </a>
-              </td>
-              <td>{r.stargazers_count}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	useEffect(() => {
+		axios
+			.get(
+				"https://api.github.com/search/repositories?q=language:javascript&sort=stars"
+			)
+			.then((res) => res.data.items)
+			.then((repos) => setRepos(repos))
+			.catch((err) => {
+				console.error(err.response.data);
+			});
+	}, []);
+
+	return (
+		<div>
+			<h2>Popular JS repos</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Stars</th>
+					</tr>
+				</thead>
+				<tbody>
+					{repos.map((r) => (
+						<tr key={r.id}>
+							<td>
+								<a target="blank" href={r.html_url}>
+									{r.name}
+								</a>
+							</td>
+							<td>{r.stargazers_count}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
--- src/SearchUsers.js
+++ src/SearchUsers.js
@@ -1,25 +1,46 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function () {
-  const [query, setQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+	const [query, setQuery] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
 
-  // TODO : fetch the github user list matching the search query
-  // (a new API call should be made every time the query changes)
-  // (errors should be logged in the console or/and printed to the UI)
+  const controller = new AbortController();
 
-  return (
-    <div>
-      <h2>Search for github users</h2>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      {searchResults.map((githubUser) => (
-        <div key={githubUser.login}>{githubUser.login} </div>
-      ))}
-    </div>
-  );
+	useEffect(() => {
+		if (query !== "") {
+			axios
+				.get(`https://api.github.com/search/users?q="${query}"`)
+				.then((res) => res.data.items)
+				.then((users) => setSearchResults(users))
+				.catch((err) => {
+					console.error(err.response.data);
+				});
+		}
+
+
+    //slide 13
+    return () => {
+      controller.abort();
+    }
+	}, [query]);
+
+	const handleChange = (e) => {
+		setQuery(e.target.value);
+		// console.log(query);
+	};
+
+	// TODO : fetch the github user list matching the search query
+	// (a new API call should be made every time the query changes)
+	// (errors should be logged in the console or/and printed to the UI)
+
+	return (
+		<div>
+			<h2>Search for github users</h2>
+			<input type="text" value={query} onChange={(e) => handleChange(e)} />
+			{searchResults.map((githubUser) => (
+				<div key={githubUser.login}>{githubUser.login} </div>
+			))}
+		</div>
+	);
 }
